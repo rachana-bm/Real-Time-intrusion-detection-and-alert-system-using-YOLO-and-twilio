823306618e1743d6864ee6fb8335060667986032
Rachan B M <rachanamadhukar11@gmail.com>
2025-08-10 10:49:43 +0530

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,4 +1,4 @@
-account_sid = 'ACae8c4f222ce4346ad1be8a079ef20d85'
-auth_token = '24910ae4b4f75d7e16bb520078e72f56'
-twilio_number = '+19205451786'
-my_number = '+91 9113539231'
+account_sid = 'ACb'
+auth_token = '33436747'
+twilio_number = '648895'
+my_number = '+916547'
